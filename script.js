4e04f1ddf6da8655929c1fa4a403bcbd18d9c470
DaweryElAsfar <bavlyazizwilliam@gmail.com>
2024-11-09 15:56:07 +0200

    script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// JavaScript for section switching
+function showSection(sectionId) {
+    document.querySelectorAll('.section').forEach(section => {
+        section.classList.remove('active');
+    });
+    document.getElementById(sectionId).classList.add('active');
+}
