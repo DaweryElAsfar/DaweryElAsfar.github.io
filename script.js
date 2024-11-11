75ad479c2e5444966ec195ff07a0fbb96f62c7d8
DaweryElAsfar <bavlyazizwilliam@gmail.com>
2024-11-11 23:18:34 +0200

    script.js

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ function showSection(sectionId) {
     });
     document.getElementById(sectionId).classList.add('active');
 }
+import { createRoot } from 'react-dom/client';
+import FantasyCupBracket from './fantasy-cup-bracket';
+
+// ... your existing code ...
+
+function showSection(sectionId) {
+  // ... your existing code ...
+  
+  if (sectionId === 'fantasy-cup') {
+    const root = createRoot(document.getElementById('fantasy-cup-root'));
+    root.render(<FantasyCupBracket />);
+  }
+}
